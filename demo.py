e72efafa20de614df483a7da6c4dc358c810a9a8
phantrdat <phantrdat97@gmail.com>
2022-03-03 11:14:46 +1100

    fix loading model failed

--- demo.py
+++ demo.py
@@ -114,8 +114,8 @@ if __name__ == '__main__':
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
     parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
     parser.add_argument('--rgb', action='store_true', help='use rgb input')
-    parser.add_argument('--character', type=str, default = '''0123456789abcdefghijklmnopqrstuvwxyz`!@#$^&*()+=-_[]/|\<>,.?:;''', help='character label')
-    parser.add_argument('--sensitive', default=True, action='store_true', help='for sensitive character mode')
+    parser.add_argument('--character', type=str, default = '''0123456789abcdefghijklmnopqrstuvwxyz''', help='character label')
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
     parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
     """ Model Architecture """
     parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
